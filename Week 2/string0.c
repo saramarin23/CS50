e0f091fb56c3211591c6c4178220154aa0cb16f7
saramarin23 <saramarin.econ@gmail.com>
2019-12-16 12:54:26 +0100

    Create string0.c

---
+++ Week 2/string0.c
@@ -0,0 +1,13 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    string s = get_string("Input: ");
+    printf("Output:\n");
+    for (int i = 0; i < strlen(s); i++)
+    {
+        printf("%c\n", s[i]);
+    }
+}
